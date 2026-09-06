5f0c591d9931ba8caead87346078f6b3a3229c8f
sagarsharma333 <mailsagarsharma333@gmail.com>
2026-09-07 03:51:04 +0530

    Post Submission changes:added DLM-LIFECYCLE for backup , fixed latest ami pulling

--- iac_terra/ec2.tf
+++ iac_terra/ec2.tf
@@ -7,10 +7,10 @@ data "aws_ami" "amazon_linux" {
         values = ["al2023-ami-*-x86_64"]
     }
   
-}
+} # this would have replaced my whole instance because of most_recent = true 
 
 resource "aws_instance" "app" {
-    ami = data.aws_ami.amazon_linux.id
+    ami = "ami-0ea4bced14b68e0bb" # hard coded for the new image changes
     instance_type = "t3.micro" #how can t3.micro be free but not t2.micro? I don't know 
     key_name = var.key_name
 
@@ -20,6 +20,10 @@ resource "aws_instance" "app" {
     vpc_security_group_ids = [aws_security_group.app.id]
     associate_public_ip_address = true
 
+    lifecycle {
+        ignore_changes = [ami]
+    }
+
     root_block_device {
         volume_size = 8
         volume_type = "gp3"
@@ -27,6 +31,7 @@ resource "aws_instance" "app" {
 
 tags = {
         Name = "${var.project_name}-app"
+        environment = "dev,prod&stg"  #added for DLM
     }
 
 }
--- iac_terra/iam.tf
+++ iac_terra/iam.tf
@@ -22,3 +22,50 @@ resource "aws_iam_instance_profile" "cloudwatch" {
     name = "${var.project_name}-cloudwatch-instance-profile"            
     role = aws_iam_role.cloudwatch.name                               # then attached it to the ec2 instance in ec2.tf
 }
+
+######### DLM Lifecycle Policy for ami Snapshots #########
+
+resource "aws_iam_role" "DLM" {
+    name = "${var.project_name}-DLM-role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "dlm.amazonaws.com"
+                }
+            },
+        ]
+    })
+  
+}
+resource "aws_iam_role_policy_attachment" "DLM" {
+    role       = aws_iam_role.DLM.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSDataLifecycleManagerServiceRole" #aws managed policy for DLM
+}
+
+
+resource "aws_dlm_lifecycle_policy" "ami_snapshot_policy" {
+    description = "DLM Lifecycle Policy for AMI Snapshots"
+    execution_role_arn = aws_iam_role.DLM.arn
+    state = "ENABLED"
+    policy_details {
+        resource_types = ["INSTANCE"]
+        #targetting a tag instead of a specific instance id fo reusability 
+        target_tags = {
+            "Environment" = "dev,prod&stg"
+        }
+
+        schedule {
+            name = "DailyAMISnapshotSchedule"
+            create_rule {
+                cron_expression = "cron(0 2 1 * ? *)" #every 1st day of the month at 2 AM UTC
+            }
+            retain_rule {
+                count = 1 # you want 2 or more for real environments
+            }
+        }
+    }
+}
